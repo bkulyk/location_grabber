89df8f9e952a1661294fe58c82bb70a11f5952d3
Brian Kulyk <brian@kulyk.ca>
2018-01-06 19:24:31 -0800

    rename file, remove unused reuires

--- features/step_definitions/location_grabber_steps.rb
+++ features/step_definitions/location_grabber_steps.rb
@@ -1,7 +1,5 @@
 # Put your step definitions here
 
-require 'awesome_print'
-
 Then("the outputfile should contain:") do |table|
   contents = File.open("tmp/aruba/output.csv", 'r') { |f| f.read() }
   table.raw.flatten.each do |check|
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,3 @@
-require 'awesome_print'
-
 ENV['PATH'] = "#{File.expand_path(File.dirname(__FILE__) + '/../../bin')}#{File::PATH_SEPARATOR}#{ENV['PATH']}"
 LIB_DIR = File.expand_path(File.join(File.expand_path(File.dirname(__FILE__)),'..','..','lib'))
 
